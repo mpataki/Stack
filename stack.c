18fff5ffc8666dc87840b2189ec70eb0dbed9c93
mpataki <matpataki@gmail.com>
2013-06-07 11:42:04 -0400

    Started implementation of stack

--- stack.c
+++ stack.c
@@ -2,3 +2,30 @@
 
 #include "stack.h"
 
+// The array will be of this size to begin with, and will
+// double as needed
+#define INITIAL_ARRAY_SIZE 50;
+
+// initialize a new stack
+stack stack_init(){
+	stack ret = malloc(sizeof(unsigned int)*INITIAL_ARRAY_SIZE);
+	ret->arrLength = INITIAL_ARRAY_SIZE;
+	ret->stackSize = 0;
+}
+
+// return the size of the stack;
+unsigned int stack_size(stack s){
+	return s->stackSize
+}
+
+// returns 1 if the stack is empty, and 0 otherwise
+int stack_empty(stack s){
+	return (s->stackSize == 0) ? 1 : 0;
+}
+
+// returns the top of the stack without removing the element
+// 0 is returned if stack is empty
+unsigned int stack_top(stack s){
+	if (s->stackSize == 0) return 0;
+	return stack->array[s->stackSize - 1];
+}
--- stack.h
+++ stack.h
@@ -1,28 +1,28 @@
 // C Stack implementation header
 
 struct stack {
-	unsigned int arrLength, size;
+	unsigned int arrLength, stackSize;
 	unsigned int* array;
 };
 
 typedef struct stack* stack;
 
-// The array will be of this size to begin with, and will
-// double as needed
-#define INITIAL_ARRAY_SIZE 50;
-
 // initialize a new stack
 stack stack_init();
 
 // return the size of the stack;
-unsigned int stack_size(stack s);
+unsigned int stack_size(stack);
+
+// returns 1 if the stack is empty, and 0 otherwise
+int stack_empty(stack);
 
 // returns the top of the stack without removing the element
-unsigned int stack_top(stack s);
+// 0 is returned if stack is empty
+unsigned int stack_top(stack);
 
-// pushes the elm onto the stack, returning 1 if successful, and 0 otherwise
-int stack_push(stack s, unsigned int elm);
+// pushes the element onto the stack, returning 1 if successful, and 0 otherwise
+int stack_push(stack, unsigned int);
 
 // returns the element on the top of the stack and removes it. 
 // 0 is returned if unsuccessful
-unsigned int stack_pop(stack s);
+unsigned int stack_pop(stack);
