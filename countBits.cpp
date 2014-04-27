5d7ac73e15883261410c6c079a32120bd00a35d9
mpataki <matpataki@gmail.com>
2014-04-27 14:29:06 -0400

    count the number of on bits in a word

---
+++ countBits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num;
+	cin >> num;
+	int count=0;
+	
+	while (num > 0){
+		if ( num % 2 == 1 ) count++;
+		num = num >> 1;
+	}
+
+	cout << count;
+	return 0;
+}
